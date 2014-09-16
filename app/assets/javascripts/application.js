258049f4561187fd06ffeeb97fe61f36a2376f83
Jeff Keslin <jkeslin@gmail.com>
2014-09-16 15:53:53 -0500

    javascript foundation debug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,12 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require turbolinks
 //= require_tree .
-$(document).foundation();
+
+$(function(){ 
+
+	$(document).foundation(); 
+
+});
