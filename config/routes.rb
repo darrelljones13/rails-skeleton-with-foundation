6ab4f4b888bbb8baea961f7b947e2050e637d7d7
Jeff Keslin <jkeslin@gmail.com>
2014-09-16 21:34:14 -0500

    add blank landing pagefor practice with foundation grid etc.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :users
 
+  get "landing" => "landings#index", :as => "landing"
 
 end
   # The priority is based upon order of creation: first created -> highest priority.
